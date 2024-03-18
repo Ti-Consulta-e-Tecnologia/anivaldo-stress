68c78316e637596ea93a765471c91490ba6f6eb6
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-03-18 19:52:14 -0300

    asd

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -11,30 +11,38 @@ export async function appRoutes(app: FastifyInstance) {
 
   app.get('/stress', async (request: FastifyRequest, reply: FastifyReply) => {
     const zonas = [
-      'fundo',
-      'patio',
-      'corredor',
-      'saida',
-      'entrada',
-      'cozinha',
-      'quarto',
-      'sala',
-      'garagem',
-      'quintal',
+      'fundo2',
+      'patio2',
+      'corredor2',
+      'saida2',
+      'entrada2',
+      'cozinha2',
+      'quarto2',
+      'sala2',
+      'garagem2',
+      'quintal2',
     ]
     const cnpjs = [
-      '07112869000177',
-      '07112869000178',
-      '19456231000145',
-      '3956234000188',
-      '9999999000199',
+      '07112869000177x',
+      '07112869000178x',
+      '19456231000145x',
+      '3956234000188x',
+      '9999999000199x',
+      '4675892142197',
+      '127812547812749821',
+      '126784124794214',
+      '12784215847921',
+      '12782164782167',
     ]
     const nomes = [
-      'nome fantasia',
-      'qualquer nome',
+      'nome fantasiado',
+      'qualquer um',
       'teste',
-      'empresa fantasma',
+      'empresa fantasminha',
       'inexistente',
+      'criacao',
+      'fantasma',
+      'faturador',
     ]
 
     const criacao = await prisma.tb_event.create({
